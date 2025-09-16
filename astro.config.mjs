4d2dafaf31ba8e65862b7757d9a41d9521b57ead
LeahMarieXOXO <leahmariecargill@gmail.com>
2025-09-16 21:29:48 +0100

    GitHub pages deployment attempt 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,11 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-site: 'https://leijahmariexoxo.github.io', 
-base: '/Final-Project---Music-by-Mood'})
+    site: 'https://leijahmariexoxo.github.io',
+    base: '/Final-Project---Music-by-Mood',
+    server: {
+        host: true,   // listens on all network interfaces
+        port: 3000,   // example port
+    }
+
+})
