8bdfa53b1bcb1934437a2eba86a6f13883b722a9
Oisín Tohak <oisintohak@gmail.com>
2025-10-01 17:29:02 +0100

    image

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://leijahmariexoxo.github.io',
-    base: '/Final-Project---Music-by-Mood',
+    site: 'https://oisin-t-ucd.github.io',
+    base: '/Final-Project---TEST',
     server: {
         host: true,   // listens on all network interfaces
         port: 3000,   // example port
